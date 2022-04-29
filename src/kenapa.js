7f0e849547d8d333012017987761af0a5d11f39e
annisaulfa11 <2011522015_annisa@student.ac.id>
2022-04-30 06:11:45 +0700

    dc

--- src/kenapa.js
+++ src/kenapa.js
@@ -1,5 +1,5 @@
 <<<<<<< HEAD
 frameElement,sssssssssssssssssssssffdaaaaaaaaaaaaaaaaa adfaj,fmndsafa nfdSsSSAafadffdkfm,s
 =======fasdffffdassssssscsdcsdfffffffffffffdsaddddddsdasffddsssssssssssss
-frameElement,f adcxxxxxxxxxxxxfaj,fmndsafa nfda
+frameElement,f cccccccccccccccccccadcxxxxxxxxxxxxfaj,fmndsafa nfda
 >>>>>>> 2896d2830a90ad0959d06bc1ea3ebf5b601f06d8
